662af0434bffb88a9038ec141bd2f9864f6ab2e4
Dynesshely <2369913161@qq.com>
2022-02-06 03:16:34 +0800

    chore(.gitignore): fix

--- obj/Debug/net6.0-windows10.0.22000.0/App.g.i.cs
+++ obj/Debug/net6.0-windows10.0.22000.0/App.g.i.cs
@@ -59,7 +59,7 @@ namespace KitX {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/KitX;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/KitX;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
