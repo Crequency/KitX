4ef89a022b2e71a3cc0f77496b6b9e56b5de7d76
Dynesshely <catrol@qq.com>
2022-06-08 07:36:27 +0800

    📦 Struct: Moved position

---
+++ KitX Dashboard/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KitX_Dashboard.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ KitX Dashboard/Views/MainWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+using FluentAvalonia.UI.Controls;
+
+namespace KitX_Dashboard.Views
+{
+    public partial class MainWindow : CoreWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
