760a842a3e6cd8ade49feb16e50df9f1fb1e8f17
Dynesshely <catrol@qq.com>
2022-06-08 06:44:37 +0800

    📦 Struct: Init Project

---
+++ KitX/KitX Dashboard/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KitX_Dashboard.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ KitX/KitX Dashboard/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace KitX_Dashboard.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
