35e9fcbb2e2a60c4bc04e87f8ea2d93736bffa42
Dynesshely <catrol@qq.com>
2022-06-13 02:34:49 +0800

    💾 Feat: Loaders

---
+++ KitX Loaders/KitX.Loader.WPF.Framework/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace KitX.Loader.WPF.Framework
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ KitX Loaders/KitX.Loader.WPF.Framework/WebClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace KitX.Loader.WPF.Framework
+{
+    public class WebClient
+    {
+
+
+        public WebClient()
+        {
+
+        }
+    }
+}
