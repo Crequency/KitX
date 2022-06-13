b3ecfa89ae14b110a16a77dc0b6922f14415226c
Dynesshely <catrol@qq.com>
2022-06-14 05:58:15 +0800

    💾 Feat: Loader 不需要窗口!

--- KitX Loaders/KitX.Loader.Winform.Core/Program.cs
+++ KitX Loaders/KitX.Loader.Winform.Core/Program.cs
@@ -11,7 +11,7 @@ namespace KitX.Loader.Winform.Core
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
         }
     }
 }
--- KitX Loaders/KitX.Loader.Winform.Framework/Program.cs
+++ KitX Loaders/KitX.Loader.Winform.Framework/Program.cs
@@ -16,7 +16,7 @@ namespace KitX.Loader.Winform.Framework
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
         }
     }
 }
