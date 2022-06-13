b1128dd90511f5fe5ec63acaaba6baa824072912
Dynesshely <catrol@qq.com>
2022-06-14 05:25:54 +0800

    💾 Feat: TestPlugin finished

---
+++ KitX Official Plugins/TestPlugin.WPF.Core/Controller.cs
@@ -0,0 +1,29 @@
+﻿using KitX.Contract.CSharp;
+
+namespace TestPlugin.WPF.Core
+{
+    public class Controller : IController
+    {
+        private readonly MainWindow mainwin;
+
+        public Controller(MainWindow mainwin)
+        {
+            this.mainwin = mainwin;
+        }
+
+        public void End()
+        {
+            mainwin.Close();
+        }
+
+        public void Pause()
+        {
+            mainwin.Hide();
+        }
+
+        public void Start()
+        {
+            mainwin.Show();
+        }
+    }
+}
---
+++ KitX Official Plugins/TestPlugin.WPF.Core/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using KitX.Contract.CSharp;
+using System;
+using System.ComponentModel.Composition;
+using System.Windows;
+
+namespace TestPlugin.WPF.Core
+{
+    [Export(typeof(IIdentityInterface))]
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, IIdentityInterface
+    {
+        private readonly Controller controller;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            controller = new(this);
+        }
+
+        public string GetAuthorLink() => "作者链接";
+
+        public string GetAuthorName() => "作者名称";
+
+        public string GetComplexDescription() => "复杂描述";
+
+        public IController GetController() => controller;
+
+        public string GetDisplayName() => "显示名称";
+
+        public DateTime GetLastUpdateDate() => DateTime.Now;
+
+        public string GetName() => "插件名称";
+
+        public DateTime GetPublishDate() => DateTime.Now;
+
+        public string GetPublisherLink() => "发行商链接";
+
+        public string GetPublisherName() => "发行者名称";
+
+        public string GetSimpleDescription() => "简单描述";
+
+        public string GetTotalDescriptionInMarkdown() => "Markdown 语法的完整描述";
+
+        public string GetVersion() => "版本";
+    }
+}
