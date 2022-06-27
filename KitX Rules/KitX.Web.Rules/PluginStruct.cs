46f3414b6388fb91e47c45d35e41e32b7b99488d
Dynesshely <catrol@qq.com>
2022-06-27 20:37:03 +0800

    💾 Feat(插件结构): 包含插件基本信息, 用于 Loader 与 Dashboard 之间传输

--- KitX Rules/KitX.Web.Rules/PluginStruct.cs
+++ KitX Rules/KitX.Web.Rules/PluginStruct.cs
@@ -1,14 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace KitX.Web.Rules
+﻿namespace KitX.Web.Rules
 {
+    /// <summary>
+    /// 插件结构
+    /// </summary>
     public struct PluginStruct
     {
         public string Name;
-        public string Description;
+
+        public string Version;
+
+        public string DisplayName;
+
+        public string AuthorName;
+
+        public string PublisherName;
+
+        public string AuthorLink;
+
+        public string PublisherLink;
+
+        public string SimpleDescription;
+
+        public string ComplexDescription;
+
+        public string TotalDescriptionInMarkdown;
+
+        public string IconInBase64;
+
+        public DateTime PublishDate;
+
+        public DateTime LastUpdateDate;
+
+        public bool IsMarketVersion;
     }
 }
