38cdf663184461ffa81566de1d87309b3efbc8d6
Dynesshely <catrol@qq.com>
2022-06-30 07:10:52 +0800

    💾 Feat(Dashboard): 插件卡片增加对 base64 编码图标的支持

---
+++ KitX Dashboard/ViewModels/Pages/LibViewModel.cs
@@ -0,0 +1,20 @@
+﻿using KitX_Dashboard.Views.Pages.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitX_Dashboard.ViewModels.Pages
+{
+    internal class LibViewModel : ViewModelBase
+    {
+        public LibViewModel()
+        {
+            for (int i = 0; i < 20; i++)
+                PluginCards.Add(new());
+        }
+
+        public static List<PluginCard> PluginCards { get; } = new();
+    }
+}
--- KitX Dashboard/Views/Pages/LibPage.axaml.cs
+++ KitX Dashboard/Views/Pages/LibPage.axaml.cs
@@ -1,13 +1,18 @@
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using KitX_Dashboard.ViewModels.Pages;
 
 namespace KitX_Dashboard.Views.Pages
 {
     public partial class LibPage : UserControl
     {
+        private readonly LibViewModel libViewModel = new();
+
         public LibPage()
         {
             InitializeComponent();
+
+            DataContext = libViewModel;
         }
 
         private void InitializeComponent()
