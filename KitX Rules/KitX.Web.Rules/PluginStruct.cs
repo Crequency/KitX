6068450f4f1b3a1db01d529522f0418005bd8c87
Dynesshely <catrol@qq.com>
2022-07-17 03:02:10 +0800

    💾 Feat: KitX 网络规则 插件结构 部分完工

--- KitX Rules/KitX.Web.Rules/PluginStruct.cs
+++ KitX Rules/KitX.Web.Rules/PluginStruct.cs
@@ -10,32 +10,32 @@
 
         }
 
-        public string Name = "Name";
+        public string Name { get; set; } = "Name";
 
-        public string Version = "Version";
+        public string Version { get; set; } = "Version";
 
-        public string DisplayName = "DisplayName";
+        public string DisplayName { get; set; } = "DisplayName";
 
-        public string AuthorName = "AuthorName";
+        public string AuthorName { get; set; } = "AuthorName";
 
-        public string PublisherName = "PublisherName";
+        public string PublisherName { get; set; } = "PublisherName";
 
-        public string AuthorLink = "AuthorLink";
+        public string AuthorLink { get; set; } = "AuthorLink";
 
-        public string PublisherLink = "PublisherLink";
+        public string PublisherLink { get; set; } = "PublisherLink";
 
-        public string SimpleDescription = "SimpleDescription";
+        public string SimpleDescription { get; set; } = "SimpleDescription";
 
-        public string ComplexDescription = "ComplexDescription";
+        public string ComplexDescription { get; set; } = "ComplexDescription";
 
-        public string TotalDescriptionInMarkdown = "TotalDescriptionInMarkdown";
+        public string TotalDescriptionInMarkdown { get; set; } = "TotalDescriptionInMarkdown";
 
-        public string IconInBase64 = "IconInBase64";
+        public string IconInBase64 { get; set; } = "IconInBase64";
 
-        public DateTime PublishDate = DateTime.Parse("2019.06.27");
+        public DateTime PublishDate { get; set; } = DateTime.Parse("2019.06.27");
 
-        public DateTime LastUpdateDate = DateTime.Parse("2022.05.02");
+        public DateTime LastUpdateDate { get; set; } = DateTime.Parse("2022.05.02");
 
-        public bool IsMarketVersion = false;
+        public bool IsMarketVersion { get; set; } = false;
     }
 }
