88e9b693286a1bcdb38a7b7e5ea1ab39bfe82b64
Dynesshely <catrol@qq.com>
2022-07-20 19:01:43 +0800

    💾 Feat(WebServer): 增加插件结构的标记, 插件断开连接时从列表中移除的部分

--- KitX Dashboard/Services/PluginsManager.cs
+++ KitX Dashboard/Services/PluginsManager.cs
@@ -6,6 +6,7 @@ using KitX_Dashboard.Views.Pages.Controls;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -21,11 +22,26 @@ namespace KitX_Dashboard.Services
         /// 执行 Socket 消息
         /// </summary>
         /// <param name="msg">消息</param>
-        internal static void Execute(string msg)
+        internal static void Execute(string msg, IPEndPoint endPoint)
         {
-            var card = new PluginCard(
-                (PluginStruct)JsonSerializer.Deserialize(msg, typeof(PluginStruct))
+            var pluginStruct = (PluginStruct)JsonSerializer.Deserialize(msg, typeof(PluginStruct));
+
+            // 标注实例注册 ID
+            pluginStruct.Tags.Add("Authorized_ID",
+                $"{pluginStruct.PublisherName}" +
+                $"." +
+                $"{pluginStruct.Name}" +
+                $"." +
+                $"{pluginStruct.Version}"
             );
+
+            // 标注 IPEndPoint
+            pluginStruct.Tags.Add("IPEndPoint", endPoint.ToString());
+
+            // 创建插件卡片
+            var card = new PluginCard(pluginStruct);
+
+            #region A foolish try.
 
             //if (Program.ViewModelPool.ContainsKey("LibViewModel"))
             //(Program.ViewModelPool["LibViewModel"] as LibViewModel).PluginCards.Add(card);
@@ -45,16 +61,31 @@ namespace KitX_Dashboard.Services
             //    Program.PluginCards.Add(card);
             //});
 
+            #endregion
+
+            // 插件卡片添加到前台
             Program.PluginCards.Add(card);
+
+            //Program.PluginsCount = $"Program.PluginCards.Count";
         }
 
         /// <summary>
         /// 断开了连接
         /// </summary>
         /// <param name="id">插件 id</param>
-        internal static void Disconnect(string id)
+        internal static void Disconnect(IPEndPoint endPoint)
         {
-
+            PluginCard? remove_target = null;
+            foreach (var item in Program.PluginCards)
+            {
+                if (item.pluginStruct.Tags["IPEndPoint"].Equals(endPoint.ToString()))
+                {
+                    remove_target = item;
+                    break;
+                }
+            }
+            if (remove_target != null)
+                Program.PluginCards.Remove(remove_target);
         }
     }
 }
