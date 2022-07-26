573436fa94c14d0ac02d466ed1b4b7d0a38033e4
Dynesshely <catrol@qq.com>
2022-07-26 11:10:48 +0800

    💾 Feat: 完成招呼语提示

--- KitX Dashboard/Models/GreetingTextGenerator.cs
+++ KitX Dashboard/Models/GreetingTextGenerator.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Avalonia;
+using System;
+
+#pragma warning disable CS8602 // 解引用可能出现空引用。
 
 namespace KitX_Dashboard.Models
 {
@@ -13,12 +12,34 @@ namespace KitX_Dashboard.Models
 
         }
 
-        internal string GetText()
+        internal static string? GetText()
         {
+            Random random = new();
             string key = $"Text_Greeting_%Step%_%Index%";
-
-            string result = "";
-            return result;
+            int time = DateTime.Now.Hour;
+            if (time >= 6 && time < 12)
+                key = key.Replace("%Step%", "Morning").Replace("%Index%",
+                    random.Next(1, Program.GlobalConfig.Config_Windows
+                    .Config_MainWindow.GreetingTextCount_Morning).ToString());
+            else if (time >= 12 && time < 14)
+                key = key.Replace("%Step%", "Noon").Replace("%Index%",
+                    random.Next(1, Program.GlobalConfig.Config_Windows
+                    .Config_MainWindow.GreetingTextCount_Noon).ToString());
+            else if (time >= 14 && time < 18)
+                key = key.Replace("%Step%", "AfterNoon").Replace("%Index%",
+                    random.Next(1, Program.GlobalConfig.Config_Windows
+                    .Config_MainWindow.GreetingTextCount_AfterNoon).ToString());
+            else if (time >= 18 && time < 24)
+                key = key.Replace("%Step%", "Evening").Replace("%Index%",
+                    random.Next(1, Program.GlobalConfig.Config_Windows
+                    .Config_MainWindow.GreetingTextCount_Evening).ToString());
+            else key = key.Replace("%Step%", "Night").Replace("%Index%",
+                    random.Next(1, Program.GlobalConfig.Config_Windows
+                    .Config_MainWindow.GreetingTextCount_Night).ToString());
+            Application.Current.Resources.MergedDictionaries[0].TryGetResource(key, out object? text);
+            return text as string;
         }
     }
 }
+
+#pragma warning restore CS8602 // 解引用可能出现空引用。
--- KitX Dashboard/ViewModels/MainWindowViewModel.cs
+++ KitX Dashboard/ViewModels/MainWindowViewModel.cs
@@ -1,25 +1,21 @@
 using KitX_Dashboard.Models;
 
-#pragma warning disable CA1822 // 将成员标记为 static
-
 namespace KitX_Dashboard.ViewModels
 {
     internal class MainWindowViewModel : ViewModelBase
     {
-        internal double DB_Width
+        internal static double DB_Width
         {
             get => Program.GlobalConfig.Config_Windows.Config_MainWindow.Window_Width;
             set => Program.GlobalConfig.Config_Windows.Config_MainWindow.Window_Width = value;
         }
 
-        internal double DB_Height
+        internal static double DB_Height
         {
             get => Program.GlobalConfig.Config_Windows.Config_MainWindow.Window_Height;
             set => Program.GlobalConfig.Config_Windows.Config_MainWindow.Window_Height = value;
         }
 
-        internal string GreetingText => new GreetingTextGenerator().GetText();
+        internal static string? GreetingText => GreetingTextGenerator.GetText();
     }
 }
-
-#pragma warning restore CA1822 // 将成员标记为 static
