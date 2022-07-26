ce12b68b383ee1ef8a48b13c1bc4e3e6e5262e1e
Dynesshely <catrol@qq.com>
2022-07-26 10:44:19 +0800

    💾 Feat(Dashboard): 初始化招呼语生成器

---
+++ KitX Dashboard/Models/GreetingTextGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitX_Dashboard.Models
+{
+    internal class GreetingTextGenerator
+    {
+        public GreetingTextGenerator()
+        {
+
+        }
+
+        internal string GetText()
+        {
+            string key = $"Text_Greeting_%Step%_%Index%";
+
+            string result = "";
+            return result;
+        }
+    }
+}
