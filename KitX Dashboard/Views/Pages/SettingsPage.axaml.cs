3167004e58c7352839e878e6ae4f003e9cb8bebc
Dynesshely <catrol@qq.com>
2022-07-27 18:36:12 +0800

    💾 Feat(Dashboard): NavigationView 对应的逻辑, 解决卡顿失败. Link #8.

--- KitX Dashboard/Views/Pages/SettingsPage.axaml.cs
+++ KitX Dashboard/Views/Pages/SettingsPage.axaml.cs
@@ -1,6 +1,13 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using BasicHelper.LiteLogger;
+using FluentAvalonia.UI.Controls;
 using KitX_Dashboard.ViewModels.Pages;
+using KitX_Dashboard.Views.Pages.Controls;
+using System;
+
+#pragma warning disable CS8602 // 解引用可能出现空引用。
+#pragma warning disable CS8601 // 引用类型赋值可能为 null。
 
 namespace KitX_Dashboard.Views.Pages
 {
@@ -13,11 +20,60 @@ namespace KitX_Dashboard.Views.Pages
             InitializeComponent();
 
             DataContext = viewModel;
+
+            InitSettingsPage();
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
         }
+
+        /// <summary>
+        /// 初始化设置页面
+        /// </summary>
+        private void InitSettingsPage()
+        {
+            this.FindControl<NavigationView>("SettingsNavigationView").SelectedItem
+                = this.FindControl<NavigationViewItem>(SelectedViewName);
+        }
+
+        /// <summary>
+        /// 前台页面切换事件
+        /// </summary>
+        /// <param name="sender">被点击的 NavigationViewItem</param>
+        /// <param name="e">路由事件参数</param>
+        private void SettingsNavigationView_SelectionChanged(object? sender,
+            NavigationViewSelectionChangedEventArgs e)
+        {
+            try
+            {
+                SelectedViewName = (
+                    (sender as NavigationView).SelectedItem as Control
+                ).Tag.ToString();
+                this.FindControl<Frame>("SettingsFrame").Navigate(SelectedViewType());
+            }
+            catch (NullReferenceException o)
+            {
+                Program.LocalLogger.Log("Logger_Debug", o.Message, LoggerManager.LogLevel.Warn);
+            }
+        }
+
+        private static string SelectedViewName
+        {
+            get => Program.GlobalConfig.Config_Pages.Config_SettingsPage.SelectedViewName;
+            set => Program.GlobalConfig.Config_Pages.Config_SettingsPage.SelectedViewName = value;
+        }
+
+        private Type SelectedViewType() => SelectedViewName switch
+        {
+            "View_General" => typeof(Settings_General),
+            "View_Performence" => typeof(Settings_Performence),
+            "View_About" => typeof(Settings_About),
+            _ => typeof(Settings_General),
+        };
     }
 }
+
+#pragma warning restore CS8601 // 引用类型赋值可能为 null。
+#pragma warning restore CS8602 // 解引用可能出现空引用。
