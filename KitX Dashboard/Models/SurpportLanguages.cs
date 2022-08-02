ecdba05a64ca811c7611462ef68e08b4267eb3e8
Dynesshely <catrol@qq.com>
2022-08-02 14:21:55 +0800

    💾 Feat(Dashboard): 受支持的语言抽象类

---
+++ KitX Dashboard/Models/SurpportLanguages.cs
@@ -0,0 +1,9 @@
+﻿namespace KitX_Dashboard.Models
+{
+    internal class SurpportLanguages
+    {
+        internal string LanguageName { get; set; } = string.Empty;
+
+        internal string LanguageCode { get; set; } = string.Empty;
+    }
+}
