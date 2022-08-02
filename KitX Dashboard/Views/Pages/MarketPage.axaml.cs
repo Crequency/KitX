9db62395b78979ccb6f1e682248f204168a4000a
Dynesshely <catrol@qq.com>
2022-08-03 02:40:41 +0800

    💾 Feat(Dashboard): 市场页面的进一步

--- KitX Dashboard/Views/Pages/MarketPage.axaml.cs
+++ KitX Dashboard/Views/Pages/MarketPage.axaml.cs
@@ -1,13 +1,18 @@
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using KitX_Dashboard.ViewModels.Pages;
 
 namespace KitX_Dashboard.Views.Pages
 {
     public partial class MarketPage : UserControl
     {
+        private readonly MarketViewModel viewModel = new();
+
         public MarketPage()
         {
             InitializeComponent();
+
+            DataContext = viewModel;
         }
 
         private void InitializeComponent()
