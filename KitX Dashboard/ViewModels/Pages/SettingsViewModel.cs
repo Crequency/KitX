82df9996c5348b58a53986d1be970385c4cf5352
Dynesshely <catrol@qq.com>
2022-08-05 04:38:16 +0800

    💾 Feat(Dashboard): 一箭穿心

--- KitX Dashboard/ViewModels/Pages/SettingsViewModel.cs
+++ KitX Dashboard/ViewModels/Pages/SettingsViewModel.cs
@@ -8,3 +8,26 @@ namespace KitX_Dashboard.ViewModels.Pages
         }
     }
 }
+
+//                       z$6*#""""*c     :@$$****$$$$L
+//                    .@$F          "N..$F         '*$$
+//                   /$F             '$P             '$$r
+//                  d$"                                #$      '%C"""$
+//                 4$F                                  $k    ud@$ JP
+//                 M$                                   J$*Cz*#" Md"
+//                 MR                              'dCum#$       "
+//                 MR                               )    $
+//                 4$                                   4$
+//                  $L                                  MF
+//                  '$                                 4$
+//                   ?B .z@r                           $
+//                 .+(2d"" ?                          $~
+//      +$c  .z4Cn*"   "$.                           $
+//  '#*M3$Eb*""         '$c                         $
+//     /$$RR              #b                      .R
+//     6*"                 ^$L                   JF
+//                           "$                 $
+//                             "b             u"
+//                               "N.        xF
+//                                 '*c    zF
+//                                    "N@"
