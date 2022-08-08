669ca79b54ae5a1a01f5f68fb4572f10ea1b225c
Dynesshely <catrol@qq.com>
2022-08-09 03:25:57 +0800

    💾 Feat(Dashboard): 显然, 本机也将显示在卡片群中

--- KitX Dashboard/ViewModels/Pages/DeviceViewModel.cs
+++ KitX Dashboard/ViewModels/Pages/DeviceViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KitX.Web.Rules;
+using KitX_Dashboard.Views.Pages.Controls;
 using Material.Icons;
 
 #pragma warning disable CS0108 // 成员隐藏继承的成员；缺少关键字 new
@@ -14,140 +17,106 @@ namespace KitX_Dashboard.ViewModels.Pages
     {
         public DeviceViewModel()
         {
-
-        }
-
-        internal double NoDevice_TipHeight { get; set; } = 300;
-
-        internal static MaterialIconKind SystemIcon
-        {
-            get
+            DeviceCards.CollectionChanged += (_, _) =>
             {
-                if (OperatingSystem.IsAndroid())
-                    return MaterialIconKind.Android;
-                if (OperatingSystem.IsBrowser())
-                    return MaterialIconKind.MicrosoftEdge;
-                if (OperatingSystem.IsFreeBSD())
-                    return MaterialIconKind.Freebsd;
-                if (OperatingSystem.IsIOS())
-                    return MaterialIconKind.AppleIos;
-                if (OperatingSystem.IsLinux())
-                    return MaterialIconKind.Linux;
-                if (OperatingSystem.IsMacCatalyst())
-                    return MaterialIconKind.AppleFinder;
-                if (OperatingSystem.IsMacOS())
-                    return MaterialIconKind.AppleFinder;
-                if (OperatingSystem.IsTvOS())
-                    return MaterialIconKind.Apple;
-                if (OperatingSystem.IsWatchOS())
-                    return MaterialIconKind.Apple;
-                if (OperatingSystem.IsWindows())
-                    return MaterialIconKind.MicrosoftWindows;
-
-                return MaterialIconKind.QuestionMarkCircle;
-
-                //return Environment.OSVersion.Platform switch
-                //{
-                //    PlatformID.Win32S => MaterialIconKind.MicrosoftWindowsClassic,
-                //    PlatformID.Win32Windows => MaterialIconKind.MicrosoftWindows,
-                //    PlatformID.Win32NT => MaterialIconKind.MicrosoftWindows,
-                //    PlatformID.WinCE => MaterialIconKind.MicrosoftWindows,
-                //    PlatformID.Unix => MaterialIconKind.Xi,
-                //    PlatformID.Xbox => MaterialIconKind.MicrosoftXbox,
-                //    PlatformID.MacOSX => MaterialIconKind.AppleFinder,
-                //    PlatformID.Other => MaterialIconKind.QuestionMark,
-                //    _ => MaterialIconKind.QuestionMark,
-                //};
-            }
+                NoDevice_TipHeight = DeviceCards.Count == 0 ? 300 : 0;
+            };
         }
 
-        internal static string SystemName
+        internal double noDevice_TipHeight = DeviceCards.Count == 0 ? 300 : 0;
+
+        internal double NoDevice_TipHeight
         {
-            get
-            {
-                if (OperatingSystem.IsAndroid())
-                    return "Android";
-                if (OperatingSystem.IsBrowser())
-                    return "Browser";
-                if (OperatingSystem.IsFreeBSD())
-                    return "FreeBSD";
-                if (OperatingSystem.IsIOS())
-                    return "IOS";
-                if (OperatingSystem.IsLinux())
-                    return "Linux";
-                if (OperatingSystem.IsMacCatalyst())
-                    return "Mac Catalyst";
-                if (OperatingSystem.IsMacOS())
-                    return "MacOS";
-                if (OperatingSystem.IsTvOS())
-                    return "TvOS";
-                if (OperatingSystem.IsWatchOS())
-                    return "WatchOS";
-                if (OperatingSystem.IsWindows())
-                    return "Windows";
-
-                return "Unknown";
-
-                //PlatformID.Win32S => "Microsoft Windows",
-                //PlatformID.Win32Windows => "Microsoft Windows",
-                //PlatformID.Win32NT => "Microsoft Windows",
-                //PlatformID.WinCE => "Microsoft Windows",
-                //PlatformID.Unix => "Unix",
-                //PlatformID.Xbox => "Xbox",
-                //PlatformID.MacOSX => "MacOSX",
-                //PlatformID.Other => "Unknown",
-                //_ => "Unknown",
-            }
-        }
-
-        internal static string SystemVersion => Environment.OSVersion.VersionString;
-
-        internal static string SystemArchitecture => Environment.Is64BitOperatingSystem ? "x64" : "x32";
-
-        internal static string DeviceInfo => $"{SystemVersion} - {SystemArchitecture}";
-
-        internal string cpu_load = string.Empty, ram_load = string.Empty,
-                        net_upload = string.Empty, net_download = string.Empty;
-
-        internal string CPU_Load
-        {
-            get => cpu_load;
+            get => noDevice_TipHeight;
             set
             {
-                cpu_load = value;
-                PropertyChanged?.Invoke(this, new(nameof(CPU_Load)));
+                noDevice_TipHeight = value;
+                PropertyChanged?.Invoke(this, new(nameof(NoDevice_TipHeight)));
             }
         }
 
-        internal string RAM_Load
-        {
-            get => ram_load;
-            set
-            {
-                ram_load = value;
-                PropertyChanged?.Invoke(this, new(nameof(RAM_Load)));
-            }
-        }
+        //internal static MaterialIconKind SystemIcon => Converters.OperatingSystem2Enum.GetOSType() switch
+        //{
+        //    OperatingSystems.Android => MaterialIconKind.Android,
+        //    OperatingSystems.Browser => MaterialIconKind.MicrosoftEdge,
+        //    OperatingSystems.FreeBSD => MaterialIconKind.Freebsd,
+        //    OperatingSystems.IOS => MaterialIconKind.AppleIos,
+        //    OperatingSystems.Linux => MaterialIconKind.Linux,
+        //    OperatingSystems.MacCatalyst => MaterialIconKind.AppleFinder,
+        //    OperatingSystems.MacOS => MaterialIconKind.AppleFinder,
+        //    OperatingSystems.TvOS => MaterialIconKind.Apple,
+        //    OperatingSystems.WatchOS => MaterialIconKind.Apple,
+        //    OperatingSystems.Windows => MaterialIconKind.MicrosoftWindows,
+        //    OperatingSystems.Unknown => MaterialIconKind.QuestionMarkCircle,
+        //    _ => MaterialIconKind.QuestionMarkCircle,
+        //};
 
-        internal string NET_Upload
-        {
-            get => net_upload;
-            set
-            {
-                net_upload = value;
-                PropertyChanged?.Invoke(this, new(nameof(NET_Upload)));
-            }
-        }
+        //internal static string SystemName => Converters.OperatingSystem2Enum.GetOSType() switch
+        //{
+        //    OperatingSystems.Android => "Android",
+        //    OperatingSystems.Browser => "Browser",
+        //    OperatingSystems.FreeBSD => "FreeBSD",
+        //    OperatingSystems.IOS => "IOS",
+        //    OperatingSystems.Linux => "Linux",
+        //    OperatingSystems.MacCatalyst => "MacCatalyst",
+        //    OperatingSystems.MacOS => "MacOS",
+        //    OperatingSystems.TvOS => "TvOS",
+        //    OperatingSystems.WatchOS => "WatchOS",
+        //    OperatingSystems.Windows => "Windows",
+        //    OperatingSystems.Unknown => "Unknown",
+        //    _ => "Unknown",
+        //};
 
-        internal string NET_Download
-        {
-            get => net_download;
-            set
-            {
-                net_download = value;
-                PropertyChanged?.Invoke(this, new(nameof(NET_Download)));
-            }
-        }
+        //internal static string SystemVersion => Environment.OSVersion.VersionString;
+
+        //internal static string SystemArchitecture => Environment.Is64BitOperatingSystem ? "x64" : "x32";
+
+        //internal static string DeviceInfo => $"{SystemVersion} - {SystemArchitecture}";
+
+        //internal string cpu_load = string.Empty, ram_load = string.Empty,
+        //                net_upload = string.Empty, net_download = string.Empty;
+
+        //internal string CPU_Load
+        //{
+        //    get => cpu_load;
+        //    set
+        //    {
+        //        cpu_load = value;
+        //        PropertyChanged?.Invoke(this, new(nameof(CPU_Load)));
+        //    }
+        //}
+
+        //internal string RAM_Load
+        //{
+        //    get => ram_load;
+        //    set
+        //    {
+        //        ram_load = value;
+        //        PropertyChanged?.Invoke(this, new(nameof(RAM_Load)));
+        //    }
+        //}
+
+        //internal string NET_Upload
+        //{
+        //    get => net_upload;
+        //    set
+        //    {
+        //        net_upload = value;
+        //        PropertyChanged?.Invoke(this, new(nameof(NET_Upload)));
+        //    }
+        //}
+
+        //internal string NET_Download
+        //{
+        //    get => net_download;
+        //    set
+        //    {
+        //        net_download = value;
+        //        PropertyChanged?.Invoke(this, new(nameof(NET_Download)));
+        //    }
+        //}
+
+        internal static ObservableCollection<DeviceCard> DeviceCards => Program.DeviceCards;
 
         public event PropertyChangedEventHandler? PropertyChanged;
     }
