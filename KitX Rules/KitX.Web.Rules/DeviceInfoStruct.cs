5c39056d181e50a63e5778f45af63b4860bded1e
Dynesshely <catrol@qq.com>
2022-08-10 17:35:44 +0800

    💾 Feat(Dashboard): 优化局域网设备发现网络传递结构

--- KitX Rules/KitX.Web.Rules/DeviceInfoStruct.cs
+++ KitX Rules/KitX.Web.Rules/DeviceInfoStruct.cs
@@ -10,20 +10,20 @@
 
         }
 
-        public DateTime SendTime { get; set; } = DateTime.Now;
-
-        public string DeviceMacAddress { get; set; } = string.Empty;
-
-        public bool IsMainDevice { get; set; } = false;
-
         public string DeviceName { get; set; } = "Unknown Device";
 
         public string DeviceOSVersion { get; set; } = "Unknown OS Version";
 
-        public OperatingSystems DeviceOSType { get; set; } = OperatingSystems.Unknown;
-
         public string IPv4 { get; set; } = "Getting...";
 
         public string IPv6 { get; set; } = "Getting...";
+
+        public string DeviceMacAddress { get; set; } = string.Empty;
+
+        public DateTime SendTime { get; set; } = DateTime.Now;
+
+        public bool IsMainDevice { get; set; } = false;
+
+        public OperatingSystems DeviceOSType { get; set; } = OperatingSystems.Unknown;
     }
 }
