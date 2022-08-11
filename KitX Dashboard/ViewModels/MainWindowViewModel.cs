4de6d896f84248c33b7a3cb413f2cb08a44d7d36
Dynesshely <catrol@qq.com>
2022-08-11 23:32:25 +0800

    🔧 Fix(Dashboard): 尝试性修复主窗体大小异常的错误

--- KitX Dashboard/ViewModels/MainWindowViewModel.cs
+++ KitX Dashboard/ViewModels/MainWindowViewModel.cs
@@ -2,17 +2,7 @@ namespace KitX_Dashboard.ViewModels
 {
     internal class MainWindowViewModel : ViewModelBase
     {
-        internal static double DB_Width
-        {
-            get => Program.GlobalConfig.Config_Windows.Config_MainWindow.Window_Width;
-            set => Program.GlobalConfig.Config_Windows.Config_MainWindow.Window_Width = value;
-        }
 
-        internal static double DB_Height
-        {
-            get => Program.GlobalConfig.Config_Windows.Config_MainWindow.Window_Height;
-            set => Program.GlobalConfig.Config_Windows.Config_MainWindow.Window_Height = value;
-        }
     }
 }
 
