298869dfd6175acdd1c2abff889efaeb183ea469
Dynesshely <catrol@qq.com>
2022-08-13 06:30:54 +0800

    📦 Struct(Installer): ...

--- KitX Installer/Installer for Windows/Resource.h
+++ KitX Installer/Installer for Windows/Resource.h
@@ -17,9 +17,10 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        131
-#define _APS_NEXT_COMMAND_VALUE         32772
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32773
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
+
