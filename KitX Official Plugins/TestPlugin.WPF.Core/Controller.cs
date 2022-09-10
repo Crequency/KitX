c91b6f90606005243006d3241a29d6e29d12916d
Dynesshely <catrol@qq.com>
2022-09-10 08:33:32 +0800

    💾 📝 Feat, Chore: 从插件获取功能列表

--- KitX Official Plugins/TestPlugin.WPF.Core/Controller.cs
+++ KitX Official Plugins/TestPlugin.WPF.Core/Controller.cs
@@ -1,4 +1,6 @@
 ﻿using KitX.Contract.CSharp;
+using KitX.Web.Rules;
+using System.Collections.Generic;
 
 namespace TestPlugin.WPF.Core
 {
@@ -24,6 +26,53 @@ namespace TestPlugin.WPF.Core
         public void Start()
         {
             mainwin.Show();
+        }
+
+        public Function GetFunctions()
+        {
+            return new Function()
+            {
+                FunctionsDisplayName = new Dictionary<string, Dictionary<string, string>>()
+                {
+                    {
+                        "Default",
+                        new Dictionary<string, string>()
+                        {
+                            { "zh-cn", "嗨嗨嗨" }
+                        }
+                    }
+                },
+                FunctionParameters = new Dictionary<string, Function.ParameterList>()
+                {
+                    {
+                        "Default",
+                        new Function.ParameterList()
+                        {
+                            HasAppendParameters = false,
+                            AppendParameterType = "",
+                            ForeParameters = new List<Function.ParameterList.Parameter>()
+                            {
+                                new Function.ParameterList.Parameter()
+                                {
+                                    DisplayName = new Dictionary<string, string>()
+                                    {
+                                        { "zh-cn", "哈哈哈" }
+                                    },
+                                    Name = "hhh",
+                                    Type = "void"
+                                }
+                            },
+                            ReturnValueType = "void"
+                        }
+                    }
+                }
+            };
+        }
+
+        public int Execute(string cmd, object arg = null)
+        {
+
+            return 0;
         }
     }
 }
