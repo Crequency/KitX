e70d8f8297dcd9f5d3fc0c154b5cedeb4e80a582
Dynesshely <catrol@qq.com>
2022-09-26 17:36:16 +0800

    🎇 Style: 运行代码清理

--- KitX Dashboard/ViewModels/Pages/Controls/Settings_UpdateViewModel.cs
+++ KitX Dashboard/ViewModels/Pages/Controls/Settings_UpdateViewModel.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
 namespace KitX_Dashboard.ViewModels.Pages.Controls
 {
-    internal class Settings_UpdateViewModel: ViewModelBase, INotifyPropertyChanged
+    internal class Settings_UpdateViewModel : ViewModelBase, INotifyPropertyChanged
     {
         internal Settings_UpdateViewModel()
         {
