57f34247bb7d35374684d29bda82208c17e0a632
Dynesshely <catrol@qq.com>
2022-09-26 16:55:15 +0800

    💾 Feat(Dashboard): 新建更新页

---
+++ KitX Dashboard/ViewModels/Pages/Controls/Settings_UpdateViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitX_Dashboard.ViewModels.Pages.Controls
+{
+    internal class Settings_UpdateViewModel: ViewModelBase, INotifyPropertyChanged
+    {
+        internal Settings_UpdateViewModel()
+        {
+
+        }
+
+
+
+        public new event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
