84500988c4b2cdf9706826c62227facd64198267
Dynesshely <catrol@qq.com>
2022-10-04 06:08:41 +0800

    💾 📝 📦 Feat, Chore, Style(Assets): 分离资产到 KitX.Assets.dll , 新建 KitX.Assets 项目

---
+++ KitX Dashboard Helper/KitX.Assets/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia.Controls;
+
+namespace KitX.Assets
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
