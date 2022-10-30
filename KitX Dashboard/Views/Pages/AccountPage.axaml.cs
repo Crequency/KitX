279362c6aa457b8525fc13e4eed0bcd0186ffdab
Dynesshely <catrol@qq.com>
2022-10-30 09:39:14 +0800

    💾 Feat(Dashboard): 开发中标识

--- KitX Dashboard/Views/Pages/AccountPage.axaml.cs
+++ KitX Dashboard/Views/Pages/AccountPage.axaml.cs
@@ -1,13 +1,18 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using KitX_Dashboard.ViewModels.Pages;
 
 namespace KitX_Dashboard.Views.Pages
 {
     public partial class AccountPage : UserControl
     {
+        private static readonly AccountPageViewModel viewModel = new();
+
         public AccountPage()
         {
             InitializeComponent();
+
+            DataContext = viewModel;
         }
 
         private void InitializeComponent()
