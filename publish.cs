6a9a814ab2931fc7230dfc953ef796e69948c0e8
Dynesshely <catrol@qq.com>
2022-12-20 12:21:27 +0800

    💾 📝 Feat, Chore: 新增一键发布脚本 在 PowerShell 中通过命令 `(Add-Type -Path "publish.cs" -PassThru)::Main()` 来执行

---
+++ publish.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Diagnostics;
+using System.IO;
+using System;
+
+namespace KitX.ToolKits
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var path = "./KitX Dashboard/";
+            var pro = "Properties/";
+            var pub = "PublishProfiles/";
+            var ab_path = Path.GetFullPath(path);
+            var ab_pub_path = Path.GetFullPath($"{path}{pro}{pub}");
+            var files = Directory.GetFiles(ab_pub_path, "*.pubxml",
+                SearchOption.AllDirectories);
+
+            foreach (var item in files)
+            {
+                var cmd = "dotnet";
+                var arg = $"" +
+                    $"publish \"{ab_path}KitX Dashboard.csproj\" " +
+                    $"\"/p:PublishProfile={item}\"";
+                Console.WriteLine($">>> Executing: {cmd} {arg}");
+                var process = new Process();
+                process.StartInfo.FileName = cmd;
+                process.StartInfo.Arguments = arg;
+                process.StartInfo.CreateNoWindow = false;
+                process.StartInfo.UseShellExecute = true;
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Minimized;
+                process.Start();
+                process.WaitForExit();
+            }
+        }
+    }
+}
