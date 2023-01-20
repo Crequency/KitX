c3ca7eaa87efb6f45c21c792c90dbe9efac368eb
Dynesshely <catrol@qq.com>
2023-01-21 03:45:18 +0800

    📝 Chore: 帮助完成代码签出

--- ToolKits/start.ps1
+++ ToolKits/start.ps1
@@ -12,9 +12,24 @@ if($type -eq "dashboard"){
     echo "executing ..."
 
     git submodule update "KitX Dashboard"
+    cd "KitX Dashboard"
+    git checkout dev=main
+    cd ".."
+
     git submodule update "KitX Dashboard Helper"
+    cd "KitX Dashboard Helper"
+    git checkout main
+    cd ".."
+
     git submodule update "KitX File Format Helper"
+    cd "KitX File Format Helper"
+    git checkout main
+    cd ".."
+
     git submodule update "KitX Rules"
+    cd "KitX Rules"
+    git checkout main
+    cd ".."
 
     echo ""
     echo "To develop <dashboard> sets, you need <reference> sets too."
@@ -27,6 +42,7 @@ if($type -eq "mobile"){
     echo "executing ..."
 
     git submodule update "KitX Mobile"
+    cd ".."
 }
 
 if($type -eq "loader"){
@@ -38,8 +54,19 @@ if($type -eq "loader"){
     echo "executing ..."
 
     git submodule update "KitX Contracts"
+    cd "KitX Contracts"
+    git checkout main
+    cd ".."
+
     git submodule update "KitX Loaders"
+    cd "KitX Loaders"
+    git checkout main
+    cd ".."
+
     git submodule update "KitX Rules"
+    cd "KitX Rules"
+    git checkout main
+    cd ".."
 }
 
 if($type -eq "plugin"){
@@ -52,9 +79,24 @@ if($type -eq "plugin"){
     echo "executing ..."
 
     git submodule update "KitX Contracts"
+    cd "KitX Contracts"
+    git checkout main
+    cd ".."
+
     git submodule update "KitX Loaders"
+    cd "KitX Loaders"
+    git checkout main
+    cd ".."
+
     git submodule update "KitX Plugins"
+    cd "KitX Plugins"
+    git checkout main
+    cd ".."
+
     git submodule update "KitX Rules"
+    cd "KitX Rules"
+    git checkout main
+    cd ".."
 }
 
 if($type -eq "installer"){
@@ -64,6 +106,9 @@ if($type -eq "installer"){
     echo "executing ..."
 
     git submodule update "KitX Installer"
+    cd "KitX Installer"
+    git checkout main
+    cd ".."
 }
 
 if($type -eq "reference"){
@@ -77,10 +122,29 @@ if($type -eq "reference"){
     echo "executing ..."
 
     git submodule update "Reference/Common.Activity"
+    cd "Reference/Common.Activity"
+    git checkout main
+    cd "../.."
+
     git submodule update "Reference/Common.Algorithm"
+    cd "Reference/Common.Algorithm"
+    git checkout dev=main
+    cd "../.."
+
     git submodule update "Reference/Common.BasicHelper"
+    cd "Reference/Common.BasicHelper"
+    git checkout dev=main
+    cd "../.."
+
     git submodule update "Reference/Common.ExternalConsole"
+    cd "Reference/Common.ExternalConsole"
+    git checkout main
+    cd "../.."
+
     git submodule update "Reference/Common.Update"
+    cd "Reference/Common.Update"
+    git checkout main
+    cd "../.."
 }
 
 
