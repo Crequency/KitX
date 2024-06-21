803c2ad1eeb5c609d6b539bf8119c747b8a5c625
Dynesshely <catrol@qq.com>
2024-06-21 20:16:40 +0800

    📝 Chore(cheese): Adapt to new cheese script engine

---
+++ .cheese/references.cs
@@ -0,0 +1,58 @@
+﻿return new List<ReferenceItem>()
+{
+    new ReferenceItem
+    {
+        Name = "Common.Activity",
+        Location = "Reference/Common.Activity",
+        Url = "git@github.com:Crequency/Common.Activity.git",
+        Branch = "dev=main",
+        RemoteBranch = "origin/dev=main",
+        Type = ReferenceType.GitRepo,
+    },
+    new ReferenceItem
+    {
+        Name = "Common.Algorithm",
+        Location = "Reference/Common.Algorithm",
+        Url = "git@github.com:Crequency/Common.Algorithm.git",
+        Branch = "dev=main",
+        RemoteBranch = "origin/dev=main",
+        Type = ReferenceType.GitRepo,
+    },
+    new ReferenceItem
+    {
+        Name = "Common.BasicHelper",
+        Location = "Reference/Common.BasicHelper",
+        Url = "git@github.com:Crequency/Common.BasicHelper.git",
+        Branch = "dev=main",
+        RemoteBranch = "origin/dev=main",
+        Type = ReferenceType.GitRepo,
+    },
+    new ReferenceItem
+    {
+        Name = "Common.Update",
+        Location = "Reference/Common.Update",
+        Url = "git@github.com:Crequency/Common.Update.git",
+        Branch = "dev=main",
+        RemoteBranch = "origin/dev=main",
+        Type = ReferenceType.GitRepo,
+    },
+    new ReferenceItem
+    {
+        Name = "Csharpell",
+        Location = "Reference/Csharpell",
+        Url = "git@github.com:Dynesshely/Csharpell.git",
+        Branch = "dev=main",
+        RemoteBranch = "origin/dev=main",
+        Type = ReferenceType.GitRepo,
+    },
+    new ReferenceItem
+    {
+        Name = "XamlMultiLanguageEditor",
+        Location = "KitX SDK/Reference/XamlMultiLanguageEditor",
+        Url = "git@github.com:Dynesshely/XamlMultiLanguageEditor.git",
+        Branch = "dev=main",
+        RemoteBranch = "origin/dev=main",
+        Type = ReferenceType.GitRepo,
+        InSubmodule = true,
+    },
+};
